b6bbc47230a1a014886f84d9eb532d1a935d0fcd
huangmocheng <53093004+huangmocheng@users.noreply.github.com>
2022-10-08 18:03:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -143,35 +143,35 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         "data": {
             "date": {
                 "value": "{} {}".format(today, week),
-                "color": get_color()
+               # "color": get_color()
             },
             "region": {
                 "value": region_name,
-                "color": get_color()
+               # "color": get_color()
             },
             "weather": {
                 "value": weather,
-                "color": get_color()
+                #"color": get_color()
             },
             "temp": {
                 "value": temp,
-                "color": get_color()
+               # "color": get_color()
             },
             "wind_dir": {
                 "value": wind_dir,
-                "color": get_color()
+               # "color": get_color()
             },
             "love_day": {
                 "value": love_days,
-                "color": get_color()
+               # "color": get_color()
             },
             "note_en": {
                 "value": note_en,
-                "color": get_color()
+                #"color": get_color()
             },
             "note_ch": {
                 "value": note_ch,
-                "color": get_color()
+                #"color": get_color()
             }
         }
     }
